456f813f32e2fbb4adbec96b1fef3e6eea8e34e6
Noizzzz-Alex <Noizzzz@yandex.ru>
2024-01-22 20:50:58 +0700

    first commit. added class Robot

---
+++ Seminar#5NewOBJ-C/Robot.h
@@ -0,0 +1,16 @@
+//
+//  Robot.h
+//  Seminar#5NewOBJ-C
+//
+//  Created by Александр Харлампов on 22.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Robot : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Seminar#5NewOBJ-C/Robot.m
@@ -0,0 +1,12 @@
+//
+//  Robot.m
+//  Seminar#5NewOBJ-C
+//
+//  Created by Александр Харлампов on 22.01.2024.
+//
+
+#import "Robot.h"
+
+@implementation Robot
+
+@end
